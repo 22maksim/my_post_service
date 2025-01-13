4e18c8d0aa3b3d5bd0bdf11b91a8c3deb7e476ef
Maks Russkikh <15maksim@mail.ru>
2025-01-13 15:34:28 +0300

    init

---
+++ src/main/java/home/my_post_service/MyPostServiceApplication.java
@@ -0,0 +1,13 @@
+package home.my_post_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/my_post_service/MyPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package home.my_post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
